4f283527551ccdd8f06ea3d21f033c03bf13eb1a
Nadav Abuhatsira <Nadav_Abuhatsira@bio-rad.com>
2025-03-21 20:30:53 +0200

    get stored jokes from local service
    2

--- src/react-app/README.md
+++ src/react-app/README.md
@@ -10,6 +10,8 @@ In the project directory, you can run:
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+google-chrome --disable-web-security --user-data-dir=~/.chrome-dev
+sudo kill -9 $(sudo lsof -t -i:3000)
 
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
--- src/react-app/src/DadJokes/DadJokesPage.tsx
+++ src/react-app/src/DadJokes/DadJokesPage.tsx
@@ -1,29 +1,38 @@
-import React,  { useState, useEffect }  from 'react';
-import axios from "axios";
-import './dad-jokes.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './dad-jokes.css';
 
 export default function DadJokesPage() {
+  const [joke, setJoke] = useState('');
 
-    const [joke, setJoke] = useState('');
+  const getNextJoke = async () => {
+    const res = await axios.get('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } });
+    setJoke(res.data.joke);
+  };
 
-    const getNextJoke = async () => {
-        const res = await axios.get('https://icanhazdadjoke.com/',  { headers: { Accept: 'application/json' }});
-        setJoke(res.data.joke);
-    }
+  const storeJoke = async () => {
+    const res = await axios.post('http://localhost:5008/jokes-api/add-joke', { joke });
+    console.log(res.data);
+  };
 
-    useEffect(() => {
-        //console.log("Dad Jokes page loaded");
-        getNextJoke();
-    }, []);
+  const getStoredJokes = async () => {
+    const res = await axios.get('http://localhost:5008/jokes-api/get-all');
+    console.log(res.data);
+  };
+
+  useEffect(() => {
+    //console.log("Dad Jokes page loaded");
+    getNextJoke();
+  }, []);
 
   return (
     <div className="dad-Joke-page">
-      <h1>
-        DadJokesPage
-      </h1>
-        <span className="joke">{joke}</span>
-        <button onClick={ getNextJoke }>Get Next Joke</button>
+      <h1>DadJokesPage</h1>
+      <span className="joke">{joke}</span>
+      <button onClick={getNextJoke}>Get Next Joke from web</button>
+
+      <button onClick={storeJoke}>Store Current Joke</button>
+      <button onClick={getStoredJokes}>Get stored jokes</button>
     </div>
   );
 }
-
