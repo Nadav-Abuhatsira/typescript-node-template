1841c8b7f90b0ab532381f5aa4a5097981c8b1a9
Nadav Abuhatsira <Nadav_Abuhatsira@bio-rad.com>
2025-03-22 13:02:09 +0200

    use router

--- src/react-app/src/App.tsx
+++ src/react-app/src/App.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+
 import './App.css';
-import DadJokesPage from "./DadJokes/DadJokesPage";
+import './style/button.css';
+import DadJokesPage from './DadJokes/DadJokesPage';
+
+// Home Page Component
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <button onClick={() => navigate('/dad-jokes')} className="button-17">
+        Go to Dad Jokes
+      </button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <DadJokesPage/>
+      <Router>
+        <nav>
+          <ul className="nav-list">
+            <li className="nav-elm">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-elm">
+              <Link to="/dad-jokes" className="nav-link">
+                Dad Jokes
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/*Implementing Routes for respective Path */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/*<Route path="/about" element={<About />}>*/}
+          {/*  <Route path="team" element={<Team />} />*/}
+          {/*  <Route path="company" element={<Company />} />*/}
+          {/*</Route>*/}
+          <Route path="/dad-jokes" element={<DadJokesPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
