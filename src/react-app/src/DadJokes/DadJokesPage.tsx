9b13582526c40393920825a0b19f1cec29ab77e3
Nadav Abuhatsira <Nadav_Abuhatsira@bio-rad.com>
2025-03-22 12:11:51 +0200

    style jokes page

--- src/react-app/src/DadJokes/DadJokesPage.tsx
+++ src/react-app/src/DadJokes/DadJokesPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import './dad-jokes.css';
+import '../style/button.css';
 import { getRandomJokeFromWeb, getStoredJokesApi, storeJokeApi } from './api/jokes-api';
 
 export default function DadJokesPage() {
@@ -41,13 +42,20 @@ export default function DadJokesPage() {
       <ToastContainer />
       <h1>DadJokesPage</h1>
       <span className="joke">{joke}</span>
-      <button onClick={getNextJoke}>Get Next Joke from web</button>
+      <div className="buttons-panel">
+        <button onClick={storeJoke} className="button-17">
+          Save Current Joke
+        </button>
+        <button onClick={getNextJoke} className="button-17">
+          Get Next Joke from web
+        </button>
+      </div>
       <h3>Jokes I like</h3>
-      {storedJokes.map((joke: any) => (
-        <span>{joke.joke}</span>
+      {storedJokes.map((joke: any, index: number) => (
+        <span key={index} className="stored-Joke">
+          {joke.joke}
+        </span>
       ))}
-      <button onClick={storeJoke}>Save Current Joke</button>
-      {/*<button onClick={getStoredJokes}>Refresh stored jokes</button>*/}
     </div>
   );
 }
